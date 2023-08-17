ae78ac6ce149c7187be08a69c8fa334ec7884574
valed-dm <dmvaled@gmail.com>
2023-08-17 19:52:48 +0300

    static data added

--- creview/settings.py
+++ creview/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "creview/static"]
+STATIC_URL = "/static/"
+STATIC_ROOT = env.str("STATIC_URL", default="static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
