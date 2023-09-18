658c2dbe681679bd1df6b20971b9db889d94c6fd
valed-dm <dmvaled@gmail.com>
2023-09-18 20:49:03 +0300

    model with a FileField is set
    uploads files to specified folder

--- files/models.py
+++ files/models.py
@@ -1,11 +1,18 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-from model_utils import Choices
+from django.db import models
+
+
+# from model_utils import Choices
+
+
+# class UserFile(models.Model):
+#     STATUS = Choices("new", "reloaded", "deleted")
+#
+#     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+#     fpath = models.CharField(max_length=300)
+#     status = models.CharField(choices=STATUS, default=STATUS.new, max_length=8)
 
 
 class File(models.Model):
-    STATUS = Choices("new", "reloaded", "deleted")
-
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    fpath = models.CharField(max_length=300)
-    status = models.CharField(choices=STATUS, default=STATUS.new, max_length=8)
+    file = models.FileField(upload_to='files/%Y/%m/%d')
