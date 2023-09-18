a100730d18a7716c110fb6ea4d7d2400e97ed7df
valed-dm <dmvaled@gmail.com>
2023-09-18 20:52:37 +0300

    upload_file func view, class based FilesListView

--- files/views.py
+++ files/views.py
@@ -1,36 +1,48 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView
+
+from files.forms import UploadFileForm
 from files.models import File
+from files.utils.get_ext import get_ext
+from files.utils.info_upload import info_upload
 
 
-# Create your views here.
+@csrf_protect
+def upload_file(request):
+    # handle file upload
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        # Only .py files allowed
+        file = request.FILES["file"]
+        fname = request.FILES['file'].name
+        file_is_py = get_ext(fname) == ".py"
+        if form.is_valid() and file_is_py:
+            new = File(user=request.user, file=file)
+            new.save()
+            info_upload(request, fname, file_is_py)
+            # Redirect to the file list
+            return HttpResponseRedirect("/files/")
+        else:
+            info_upload(request, fname, file_is_py)
+    else:
+        # An empty form
+        form = UploadFileForm()
+
+    return render(request, "files/upload.html", {"form": form})
+
+
 class FilesListView(ListView):
     model = File
+    context_object_name = 'files_list'
     template_name = "files/files.html"
-    # context_object_name = "file"
 
-    # def get(self, request, *args, **kwargs):
-    #     if "del" in request.GET:
-    #         delete_book(request)
-    #     return super().get(self, *args, *kwargs)
-    #
-    # def post(self, request, *args, **kwargs):
-    #     return super().get(self, *args, *kwargs)
-    #
-    # def get_queryset(self):
-    #     lib = library_to_view(self.request)
-    #     req = ArgsLibrary()
-    #     if self.request.method == "POST":
-    #         req = get_args_library(self.request)
-    #     books_cards_sorted = sort_books(req, library_books=lib.books_cards)
-    #     lib.books_cards = books_cards_sorted
-    #     info_sort_library(self.request, req)
-    #     return lib
-    #
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super().get_context_data(*args, **kwargs)
-    #
-    #     # book image size: width, height
-    #     context["width"] = 105
-    #     context["height"] = 150
-    #
-    #     return context
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['files_list'] = File.objects.filter(user=self.request.user)
+        return context
+
+
+class ReportView(ListView):
+    template_name = "files/report.html"
