e5dc51af52c8776eaa14927b5f78ad3a7448f761
valed-dm <dmvaled@gmail.com>
2023-09-21 20:52:07 +0300

    models.py modified for
    dynamic upload_to=filepath creation with year/month/date structure returned by user_directory_path

--- files/models.py
+++ files/models.py
@@ -1,18 +1,25 @@
+"""Files app models"""
+
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
 
-# from model_utils import Choices
+def user_directory_path(instance, file):
+    """Prepares a path to save file in date structured folder"""
 
-
-# class UserFile(models.Model):
-#     STATUS = Choices("new", "reloaded", "deleted")
-#
-#     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-#     fpath = models.CharField(max_length=300)
-#     status = models.CharField(choices=STATUS, default=STATUS.new, max_length=8)
+    year = datetime.today().year
+    month = datetime.today().month
+    day = datetime.today().day
+    return 'user_{0}/{1}/{2}/{3}/{4}'.format(instance.user.id, year, month, day, file)
 
 
 class File(models.Model):
+    """The main files table"""
+
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    file = models.FileField(upload_to='files/%Y/%m/%d')
+    file_name = models.CharField(max_length=50, null=False, blank=False)
+    file = models.FileField(upload_to=user_directory_path)
+    date = models.DateTimeField(auto_now_add=True, blank=False)
+    status = models.CharField(max_length=8, default="new")
