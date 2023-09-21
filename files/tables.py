7cde57af1c31bbadca4a43495f089e4222fb6a5c
valed-dm <dmvaled@gmail.com>
2023-09-21 21:00:11 +0300

    class FilesTable(tables.Table) created to
    present File table in the view

---
+++ files/tables.py
@@ -0,0 +1,20 @@
+"""Contains files app tables schemas"""
+
+import django_tables2 as tables
+
+from files.models import File
+
+
+class FilesTable(tables.Table):
+    """Creates uploaded files table to be presented by view"""
+
+    file = tables.FileColumn(attrs={
+        "td": {"class": "bg-success"},
+        "a": {"style": "color: white;"},
+    })
+
+    class Meta:
+        """Metadata for FilesTable"""
+
+        model = File
+        fields = ['file', 'date', 'status']
