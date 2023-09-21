b94ae244dbe02d392f8479f50b10b561af492360
valed-dm <dmvaled@gmail.com>
2023-09-21 21:01:59 +0300

    info_upload messages are shortened

--- files/utils/info_upload.py
+++ files/utils/info_upload.py
@@ -5,11 +5,11 @@ def info_upload(request, file_name, file_is_py):
     if file_is_py:
         messages.info(
             request,
-            f"'{file_name}' was successfully uploaded to "
+            f"'{file_name}' is uploaded to "
             f"{request.user.email}\'s list."
         )
     else:
         messages.info(
             request,
-            f"Upload failed. '{file_name}' doesn't end with '.py'"
+            f"Upload failed for '{file_name}'"
         )
