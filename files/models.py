93901e320350757a2a18f8c418fe109504660ad4
valed-dm <dmvaled@gmail.com>
2023-09-22 20:15:32 +0300

    remove file option implemented
    files are removed themselves from media folder and db records

--- files/models.py
+++ files/models.py
@@ -1,9 +1,19 @@
-"""Files app models"""
+"""Files app models
 
+The interesting feature here is the if statement filters for empty directories
+that are leaves on the filesystem tree. os.removedirs() deletes all empty folders
+in above an empty leaf. If there are several empty leaves on a branch,
+deleting the last empty leaf will cause os.removedirs() to walk up the branch.
+So all empty dirs are gone in a single iteration of the loop with no recursion necessary!
+"""
+import os
 from datetime import datetime
+from pathlib import Path
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 def user_directory_path(instance, file):
@@ -23,3 +33,20 @@ class File(models.Model):
     file = models.FileField(upload_to=user_directory_path)
     date = models.DateTimeField(auto_now_add=True, blank=False)
     status = models.CharField(max_length=8, default="new")
+
+
+@receiver(pre_delete, sender=File)
+def remove_file(**kwargs):
+    """Removes files from media folder"""
+
+    # db record clearance
+    instance = kwargs.get('instance')
+    instance.file.delete(save=False)
+    # media folder empty dirs clearance
+    try:
+        for p in Path("media").glob('**/*'):
+            if p.is_dir() and len(list(p.iterdir())) == 0:
+                os.removedirs(p)
+    except FileNotFoundError:
+        pass
+
