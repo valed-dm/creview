316d7f539106fa7f0905ccfd0e63994f751eede0
valed-dm <dmvaled@gmail.com>
2023-09-26 23:14:44 +0300

    files views simplified
    handle_uploaded_file, get_df, get_csv_table allocated in files/utils

--- files/views.py
+++ files/views.py
@@ -1,21 +1,18 @@
 """Files app views"""
-import csv
-import io
 import os
 
 import django_tables2 as tables
-import pandas as pd
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
 
 from files.forms import UploadFileForm
+from files.handlers.handle_uploaded_file import handle_uploaded_file
 from files.models import File
 from files.tables import FilesTable
-from files.utils.get_ext import get_ext
-from files.utils.get_table import get_table_from_headers
-from files.utils.info_upload import info_upload
+from files.utils.get_csv_table import get_csv_table
+from files.utils.get_df import get_df
 
 
 @csrf_protect
@@ -24,51 +21,10 @@ def upload_file(request):
 
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
-
-        user = request.user
-        file = request.FILES["file"]
-        fname = file.name
-        is_csv = get_ext(fname) == ".csv"
-
-        # Only {'.csv'} files allowed
-        if form.is_valid() and is_csv:
-            # searches db among user's files for a match
-            prev_file = File.objects \
-                .filter(file_name=fname).all() \
-                .filter(user=user).first()
-            if not prev_file:
-                f = file.read().decode('utf-8')
-                reader = csv.DictReader(io.StringIO(f))
-                fieldnames = ", ".join(reader.fieldnames)
-                new = File(
-                    file=file,
-                    file_name=fname,
-                    headers=fieldnames,
-                    user=user
-                )
-                new.save()
-            else:
-                # deletes existing file
-                prev_file_path = prev_file.file.path
-                os.remove(prev_file_path)
-                prev_file.delete()
-                # prepares file record stamped as 'reviewed'
-                reviewed = File(
-                    file=file,
-                    file_name=fname,
-                    user=user,
-                    status="reviewed"
-                )
-                # uploads a new reviewed file version
-                reviewed.save()
-
-            info_upload(request, fname, is_csv)
-            return HttpResponseRedirect("/files_table/")
-
-        else:
-            info_upload(request, fname, is_csv)
+        if form.is_valid():
+            if handle_uploaded_file(request):
+                return HttpResponseRedirect("/files_table/")
     else:
-        # An empty form
         form = UploadFileForm()
 
     return render(request, "files/upload.html", {"form": form})
@@ -78,23 +34,17 @@ def csv_table(request):
     """Handles .csv file view as a table"""
 
     csv_f = request.GET.get('f')
-
-    if csv_f.startswith("https"):
+    # to handle external links contained in csv table
+    if csv_f.startswith("https") or csv_f.startswith("http"):
         return HttpResponseRedirect(csv_f)
+    df = get_df(csv_f)
+    table = get_csv_table(request, df)
 
-    filename = os.path.basename(csv_f)
-    df = pd.read_csv(csv_f)
-    headers = []
-    for col in df.columns:
-        headers.append(col)
-    dynamic_table = get_table_from_headers(headers)
-    table = dynamic_table(
-        data=df.to_dict("records"),
-        template_name="django_tables2/bootstrap5.html"
-    )
-    table.paginate(page=request.GET.get("page", 1), per_page=25)
-
-    context = {'csv_table': table, 'filename': filename, 'fpath': csv_f}
+    context = {
+        'csv_table': table,
+        'filename': os.path.basename(csv_f),
+        'fpath': csv_f
+    }
 
     return render(request, "files/csv_table.html", context)
 
