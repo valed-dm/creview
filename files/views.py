4eb30551805307fc14869e7d0ad5bbe0aacee63e
valed-dm <dmvaled@gmail.com>
2023-09-26 06:57:48 +0300

    csv_table view added
    also fieldnames are get at file loading stage when db record is being created

--- files/views.py
+++ files/views.py
@@ -1,43 +1,49 @@
 """Files app views"""
-import json
+import csv
+import io
 import os
 
 import django_tables2 as tables
+import pandas as pd
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
-from django.views.generic import ListView
 
 from files.forms import UploadFileForm
 from files.models import File
 from files.tables import FilesTable
 from files.utils.get_ext import get_ext
+from files.utils.get_table import get_table_from_headers
 from files.utils.info_upload import info_upload
 
 
 @csrf_protect
 def upload_file(request):
-    """Handle file upload"""
+    """Handles file upload"""
 
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
 
         user = request.user
         file = request.FILES["file"]
-        fname = request.FILES['file'].name
-        file_is_py = get_ext(fname) == ".py"
+        fname = file.name
+        is_csv = get_ext(fname) == ".csv"
 
-        # Only {'.py'} files allowed
-        if form.is_valid() and file_is_py:
+        # Only {'.csv'} files allowed
+        if form.is_valid() and is_csv:
             # searches db among user's files for a match
             prev_file = File.objects \
                 .filter(file_name=fname).all() \
                 .filter(user=user).first()
             if not prev_file:
+                f = file.read().decode('utf-8')
+                reader = csv.DictReader(io.StringIO(f))
+                fieldnames = ", ".join(reader.fieldnames)
                 new = File(
                     file=file,
                     file_name=fname,
+                    headers=fieldnames,
                     user=user
                 )
                 new.save()
@@ -56,11 +62,11 @@ def upload_file(request):
                 # uploads a new reviewed file version
                 reviewed.save()
 
-            info_upload(request, fname, file_is_py)
+            info_upload(request, fname, is_csv)
             return HttpResponseRedirect("/files_table/")
 
         else:
-            info_upload(request, fname, file_is_py)
+            info_upload(request, fname, is_csv)
     else:
         # An empty form
         form = UploadFileForm()
@@ -68,20 +74,43 @@ def upload_file(request):
     return render(request, "files/upload.html", {"form": form})
 
 
+def csv_table(request):
+    """Handles .csv file view as a table"""
+
+    csv_f = request.GET.get('f')
+
+    if csv_f.startswith("https"):
+        return HttpResponseRedirect(csv_f)
+
+    filename = os.path.basename(csv_f)
+    df = pd.read_csv(csv_f)
+    headers = []
+    for col in df.columns:
+        headers.append(col)
+    dynamic_table = get_table_from_headers(headers)
+    table = dynamic_table(data=df.to_dict("records"))
+
+    context = {'csv_table': table, 'filename': filename, 'fpath': csv_f}
+
+    return render(request, "files/csv_table.html", context)
+
+
 @method_decorator(csrf_protect, 'post')
 class FilesTableView(tables.SingleTableView):
-    """Provides uploaded files table view """
+    """Provides table view for uploaded files"""
 
     table_class = FilesTable
     template_name = "files/files_table.html"
 
-    # post-method allowed for FilesTableView
     def post(self, request, *args, **kwargs):
+        """Allows post-method for a view"""
+
         return super().get(self, *args, *kwargs)
 
     def get_queryset(self):
-        """Filters output for user's files"""
+        """Outputs current user files"""
 
+        # deletes uploaded files from user's media folder on request
         if self.request.method == "POST":
             pks = self.request.POST.getlist("delete")
             selected_files = File.objects.filter(pk__in=pks)
@@ -90,21 +119,3 @@ class FilesTableView(tables.SingleTableView):
         return File.objects \
             .filter(user=self.request.user) \
             .order_by("-date")
-
-
-class ReportView(ListView):
-    """Provides report view"""
-
-    template_name = "files/report.html"
-
-# class FilesListView(ListView):
-#     model = File
-#     context_object_name = 'files_list'
-#     template_name = "files/files.html"
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['files_list'] = File.objects \
-#             .filter(user=self.request.user) \
-#             .order_by("-date")
-#         return context
