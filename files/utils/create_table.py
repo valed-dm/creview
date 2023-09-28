68fedbac97a5741370d5709d8e153c58fac95d75
valed-dm <dmvaled@gmail.com>
2023-09-28 22:35:26 +0300

    create_table.py manages links, checkboxes

--- files/utils/create_table.py
+++ files/utils/create_table.py
@@ -4,18 +4,36 @@ as django_tables2 table on the base of a csv header's list
 """
 import django_tables2 as tables
 
+from files.tables import CheckBoxColumnWithName
 
-def create_table_from_headers(headers: list) -> type:
+
+def create_table_from_headers(headers: list, links: list) -> type:
     """Creates CSVTable class from a list of .csv file headers"""
 
     attrs = {}
+
+    if links is None:
+        links = []
+
     for item in headers:
-        if item.lower() in ["links", "image", "reference", "website", "wikipedia"]:
-            # creates columns with clickable external links
+        if item in links:
+            # columns with clickable external links
             attrs[str(item)] = tables.URLColumn(
                 attrs={"a": {"style": "color: white;"}},
             )
+        elif item in ["include", "as_link"]:
+            # checkboxes for table view customization
+            attrs[str(item)] = CheckBoxColumnWithName(
+                verbose_name=item,
+                accessor='columns',
+                attrs={
+                    "th": {"class": "text-success"},
+                    "input": {"style": "height: 13px"}
+                },
+                empty_values=()
+            )
         else:
+            # text columns
             attrs[str(item)] = tables.Column()
 
     # returns dynamically created django_tables2 CSVTable class
