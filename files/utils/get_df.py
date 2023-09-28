b3f7e2686a7489a094b8f9a561329cb1aac5569c
valed-dm <dmvaled@gmail.com>
2023-09-28 22:38:16 +0300

    usecols arg added to get_df.py

--- files/utils/get_df.py
+++ files/utils/get_df.py
@@ -2,12 +2,17 @@
 import pandas as pd
 
 
-def get_df(path):
+def get_df(path, usecols=lambda x: x):
     """Handles oversized files too"""
 
     chunksize = 100000
     # TextFileReader object
-    tfr = pd.read_csv(path, chunksize=chunksize, iterator=True)
+    tfr = pd.read_csv(
+        path,
+        chunksize=chunksize,
+        usecols=usecols,
+        iterator=True
+    )
     # chunks are being concatenated here
     df = pd.concat(tfr, ignore_index=True)
 
