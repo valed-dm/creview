ed51a3a3323e64bd587de74f64e15cae3d5a98c1
valed-dm <dmvaled@gmail.com>
2023-10-03 23:34:15 +0300

    filter_by_rows_values.py handler created

---
+++ files/handlers/filter_by_rows_values.py
@@ -0,0 +1,25 @@
+"""
+Multiple rows filtering
+"""
+
+
+def filter_by_rows_values(val_dict, dataframe, not_include):
+    """Executes multiple row search with one or many column's values"""
+
+    # df columns headers all lowercase
+    dataframe.columns = [
+        str(x).lower().replace(" ", "_") for x in dataframe.columns
+    ]
+    # gets column header (key) from dictionary
+    key = list(val_dict)[0]
+
+    # .isin() considers multiple column values rows contain
+    mask = dataframe[key].isin(val_dict[key])
+
+    # invert mask to exclude rows if requested
+    if not_include:
+        mask = ~mask
+
+    res = dataframe[mask]
+
+    return res
