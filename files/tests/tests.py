5dc0e9c2332e60f69da66d370554b56821982906
valed-dm <dmvaled@gmail.com>
2023-10-04 09:52:17 +0300

    test user added to FileTestCase setup

--- files/tests/tests.py
+++ files/tests/tests.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from files.models import File
 
 
 class FileTestCase(TestCase):
 
     def setUp(self):
-        pass
+        user = get_user_model().objects.create_user(
+            username='test',
+            email='test@example.com',
+            password='passwordtest'
+        )
